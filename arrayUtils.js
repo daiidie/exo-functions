5a6d7955bc679d2f95a8089f9bac73772274f93f
daiidie <delavegae46@gmail.com>
2021-03-05 09:29:07 +0100

    ...

---
+++ arrayUtils.js
@@ -0,0 +1,5 @@
+const biggest = (numbers) =>{
+  return Math.max(numbers)
+}
+  console.log(biggest([100,1,200,52,63,87,99]))
+
---
+++ calc.js
@@ -0,0 +1,23 @@
+const add = (nb1, nb2) => {
+return nb1 + nb2 
+}
+console.log(add(5,6))
+
+const sub = (nb3, nb4) => {
+  return nb3 - nb4
+}
+console.log(sub(10,3))
+
+const mul = (nb5, nb6) => {
+  return nb5 * nb6
+}
+console.log(mul(25,4))  
+
+const div = (nb7, nb8) => {
+  return nb7 / nb8
+}
+console.log(div(150,5))  
+
+const calc = () => {
+  
+}
