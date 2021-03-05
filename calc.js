17b6c30bc4a419e5c8e6832945b020bf6cedf4a2
daiidie <delavegae46@gmail.com>
2021-03-05 12:05:14 +0100

    fix

--- calc.js
+++ calc.js
@@ -3,21 +3,41 @@ return nb1 + nb2
 }
 console.log(add(5,6))
 
-const sub = (nb3, nb4) => {
-  return nb3 - nb4
+const sub = (nb1, nb2) => {
+  return nb1 - nb2
 }
 console.log(sub(10,3))
 
-const mul = (nb5, nb6) => {
-  return nb5 * nb6
+const mul = (nb1, nb2) => {
+  return nb1 * nb2
 }
 console.log(mul(25,4))  
 
-const div = (nb7, nb8) => {
-  return nb7 / nb8
+const div = (nb1, nb2) => {
+  return nb1 / nb2
 }
 console.log(div(150,5))  
 
-const calc = () => {
-  
+const calc = (operator,nb1, nb2) => {
+   switch(operator) {
+     case "+":
+     return nb1 + nb2 
+     break;
+     case "-":
+     return nb1 - nb2 
+     break;
+     case "*":
+     return nb1 * nb2 
+     break;
+     case "/":
+     return nb1 / nb2 
+     break;
+     default: 
+     console.log("valeur non connue")
+     
+    } 
+
+
+
 }
+console.log(calc("+", 10, 5 ))
