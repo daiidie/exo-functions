68d2e790eafb1b2d3c68ec0ce21c5360e2dccf11
daiidie <delavegae46@gmail.com>
2021-03-05 10:45:51 +0100

    Corrigé

--- arrayUtils.js
+++ arrayUtils.js
@@ -1,5 +1,16 @@
-const biggest = (numbers) =>{
-  return Math.max(numbers)
+const biggest = (tab) =>{
+  let MAX = tab[0]
+  for(const elem of tab) {
+    if (MAX < elem) {
+      MAX = elem
+    }
+  }
+  return MAX
 }
   console.log(biggest([100,1,200,52,63,87,99]))
 
+  // autre méthode
+/*
+const biggest = (tabNb) => {
+  return Math.max(...tabNb)}
+*/
